dd56c1ed3c6b52b39884dd43825818bc08ec1af2
My <myglimsjo@gmail.com>
2025-03-20 07:33:27 +0100

    Update of tech

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,11 @@
-import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), tsconfigPaths()],
   server: {
-    middlewareMode: true,
+    middlewareMode: false,
   },
   build: {
     rollupOptions: {
